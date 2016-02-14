859bed2991584fdcf1d2cb6af92153032c3e7c10
Shayla Fitzpatrick <shayla.i.love.books@gmail.com>
2016-02-14 17:23:42 -0500

    Create HelloWorld.m

---
+++ HelloWorld.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HellotoyouWorld
+//
+//  Created by Shayla Fitzpatrick on 2/4/16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+        NSLog(@"Hello, World!");
+   
+}
